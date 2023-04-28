6332a4c293334a08f6498da9645297b6973301bc
Maria Loginova <marialog@yandex.ru>
2023-04-28 14:23:35 +0300

    added junit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,26 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldCorrectAmount() {
+        assertEquals(service.remain(4250), 750);
+
+    }
+
+    @Test
+    public void shouldAmountZero() {
+        assertEquals(service.remain(0), 1000);
+    }
+
+    @Test
+    public void shouldBuyExtraIfTheAmount1000() {
+        assertEquals(service.remain(1000), 0);
+    }
+}
